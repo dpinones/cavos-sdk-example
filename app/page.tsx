1194d78b3618d65a860f174b81cfb7090494b10a
Damian Piñones <pinonesdamian@gmail.com>
2025-08-16 11:12:23 -0300

    Update location permission error message in Home component for iPhone users
    - Added clarification that distances will not be displayed if location access is denied.
    - Enhanced guidance for enabling location services in the browser for iPhone users.

--- app/page.tsx
+++ app/page.tsx
@@ -125,7 +125,7 @@ export default function Home() {
           case error.PERMISSION_DENIED:
             const isIPhone = /iPhone/.test(navigator.userAgent);
             if (isIPhone) {
-              setMessage('Para activar la localización en tu navegador, abre la app Configuración en tu iPhone, ve a Privacidad y seguridad > Localización, busca tu navegador en la lista de apps y selecciona "Al usar la app".');
+              setMessage('Las distancias no se mostrarán. Para activar la localización en tu navegador, abre la app Configuración en tu iPhone, ve a Privacidad y seguridad > Localización, busca tu navegador en la lista de apps y selecciona "Al usar la app".');
               setTimeout(() => setMessage(''), 12000);
             } else {
               setMessage('Permiso de ubicación denegado. Las distancias no se mostrarán.');
